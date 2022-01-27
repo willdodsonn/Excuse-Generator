ba7c62a903aec711de16477b08b370632f9419c4
willdodsonn <sodlax38@gmail.com>
2022-01-27 03:14:18 +0000

    NEw Excuse Generator

--- src/app.js
+++ src/app.js
@@ -9,30 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
+const excuseGenerator = () => {
+  let who = ["I", "He", "They", "Sally"];
+  let action = [
+    "slept through",
+    "sang karaoke",
+    "went skydiving",
+    "ate broccoli"
   ];
-  return (
+  let what = ["my alarm", "in the freezer", "on the dishwasher"];
+  let result =
     who[Math.floor(Math.random() * who.length)] +
     " " +
     action[Math.floor(Math.random() * action.length)] +
     " " +
-    what[Math.floor(Math.random() * what.length)] +
-    " " +
-    when[Math.floor(Math.random() * when.length)] +
-    " "
-  );
+    what[Math.floor(Math.random() * what.length)];
+  return result;
 };
-
-//let random = who.length;
-//console.log(who[Math.floor(Math.random() * random)]);
-
-document.getElementById("excuse").innerHTML = generateExcuse();
+document.getElementById("excuse").innerHTML = excuseGenerator();
